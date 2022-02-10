a209b4a1bd1e1810435655c9845e21dabb62be3b
Gaijin81 <stephane.gagnant@sncf.fr>
2022-02-10 11:27:57 +0100

    Moodday10022022

--- moodmoovproject/src/components/Moodday.js
+++ moodmoovproject/src/components/Moodday.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "../styles/Components/moodday.scss"
+import Mooddaypopup from './Mooddaypopup';
 
 const Moodday = () => {
     return (
         <div>
             <h2>Mood du jour</h2>
             <div id="contenu" className="moodimg">
-            
 		        <button className="imgMaison" >A la Maison</button>
 		        <button className="imgFetard">Fétard</button>
 		        <button className="imgAmoureux">En Amoureux</button>
