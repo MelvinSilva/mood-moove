8f1b1e616faceec03e7dbad9455d5dc1954930e5
Leila <leilaaa77220@gmail.com>
2022-02-11 15:55:42 +0100

    correction

--- moodmoovproject/src/pages/Ticket.js
+++ moodmoovproject/src/pages/Ticket.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import Mooddaypopup from '../components/Moodday/Mooddaypopup';
 import BlockPrinc from '../components/Descriptif/BlockPrinc';
 import Moodday from '../components/Moodday/Moodday';
 import Footer from '../components/Footer';
