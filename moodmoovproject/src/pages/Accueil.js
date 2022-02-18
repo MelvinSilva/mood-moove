4bc7117c2b868249765eed713f510e4cad8f80f5
Leila <leilaaa77220@gmail.com>
2022-02-18 10:36:19 +0100

    resolve conflit

--- moodmoovproject/src/pages/Accueil.js
+++ moodmoovproject/src/pages/Accueil.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer';
 import Moodday from '../components/Moodday/Moodday';
 import Main from '../components/Main';
 import Header from '../components/Header';
+import Mooddaypopup2 from '../components/Moodday/Mooddaypopup2';
 
 
 
@@ -12,7 +13,8 @@ const Accueil = () => {
             <Header />
             <Main />
             <Moodday />
-            <Footer />   
+            <Footer />
+            <Mooddaypopup2 />   
         </div>
     );
 };
