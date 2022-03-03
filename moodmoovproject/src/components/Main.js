dd8753f0d8ba2fe5c4be95571f6db2c00e167534
Leila <leilaaa77220@gmail.com>
2022-03-03 09:51:15 +0100

    changement git commit -m changement git commit -m changement

--- moodmoovproject/src/components/Main.js
+++ moodmoovproject/src/components/Main.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import backgroundVideo from '../video/arc6.mp4';
 import BlocUnik from './BlocUnik';
+
 
 const Main = () => {
     return (
+        
         <div className='Home'>
-            <BlocUnik />
-        </div>
+            
+            <video autoPlay loop muted id='video'>
+            <source src={backgroundVideo} type='video/mp4'/>
+            </video>
+         <BlocUnik />
+       </div>
+       
     );
 };
 
--- moodmoovproject/src/pages/Accueil.js
+++ moodmoovproject/src/pages/Accueil.js
@@ -3,11 +3,7 @@ import Footer from '../components/Footer';
 import Moodday from '../components/Moodday/Moodday';
 import Main from '../components/Main';
 import Header from '../components/Header';
-<<<<<<< HEAD
-import Mooddaypopup2 from '../components/Moodday/Mooddaypopup2';
-=======
 import TestAPI from '../components/TestAPI';
->>>>>>> c499aaa1bc2ce9866e08b72ef9d20c983e663e88
 
 
 
@@ -17,13 +13,8 @@ const Accueil = () => {
             <Header />
             <Main />
             <Moodday />
-<<<<<<< HEAD
-            <Footer />
-            <Mooddaypopup2 />   
-=======
             <Footer />   
             <TestAPI />
->>>>>>> c499aaa1bc2ce9866e08b72ef9d20c983e663e88
         </div>
     );
 };
