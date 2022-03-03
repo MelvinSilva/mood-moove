9fc217605c72efeb21fcba1bbbc4c3abe76fa960
Gaijin81 <stephane.gagnant@sncf.fr>
2022-03-03 16:39:16 +0100

    config couleur bouton

--- moodmoovproject/src/components/DemoCarousel.js
+++ moodmoovproject/src/components/DemoCarousel.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { useContext } from 'react';
 import DisplayApi from './DisplayApi';
